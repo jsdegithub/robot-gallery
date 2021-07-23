5ad580a82099a151d0795d7b465c5caea3a04056
jsdegithub <def_myself@qq.com>
2021-07-23 18:40:01 +0800

    使用自定义钩子抽取组件的重复逻辑

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { appSetStateContext } from "../appState";
-import { RobotProps } from "./Robot";
+import { RobotProps } from "./RobotDiscount";
 
 export const whithAddToCart = (ChildComponent: React.ComponentType<RobotProps>) => {
   return (props) => {
@@ -19,4 +19,21 @@ export const whithAddToCart = (ChildComponent: React.ComponentType<RobotProps>) 
     };
     return <ChildComponent {...props} addToCart={addToCart}></ChildComponent>;
   };
+};
+
+export const useAddToCart = () => {
+  const setState = useContext(appSetStateContext);
+  const addToCart = (id, name) => {
+    if (setState) {
+      setState((state) => {
+        return {
+          ...state,
+          shoppingCart: {
+            items: [...state.shoppingCart.items, { id, name }],
+          },
+        };
+      });
+    }
+  };
+  return addToCart;
 };
--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,17 +1,17 @@
 import React, { useContext } from "react";
 import { appContext } from "../appState";
 import styles from "./Robot.module.css";
-import {whithAddToCart} from './AddToCart'
+import { useAddToCart } from "./AddToCart";
 
 export interface RobotProps {
   id: number;
   name: string;
   email: string;
-  addToCart: (id, name) => void;
 }
 
-const Robot: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
   const value = useContext(appContext);
+  const addToCart = useAddToCart();
   return (
     <div className={styles.cardContainer}>
       <img alt="robot" src={`https://robohash.org/${id}`} />
@@ -29,4 +29,4 @@ const Robot: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
   );
 };
 
-export default whithAddToCart(Robot);
+export default Robot;
